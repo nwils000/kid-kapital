aa20370309e4e06559644a833a84b284b96d8799
nwils000 <nwils000@gmail.com>
2024-06-13 20:51:00 -0400

    tried hacking it!

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,27 @@
 import '../styles/landing-page.css';
-import LandingPageNavbar from '../layout/LandingPageNavbar.jsx';
+import '../styles/navbar.css';
+import { Link } from 'react-router-dom';
+
+function LandingPageNavbar() {
+  return (
+    <>
+      <div className="navbar">
+        <Link to="/">
+          <span className="logo-text">Kid Kapital</span>
+        </Link>
+        <div className="nav-links-wrapper">
+          <Link className="nav-link" to="/register">
+            Register
+          </Link>
+          <Link to="/login" className="nav-link">
+            Sign in
+          </Link>
+        </div>
+      </div>
+      <div style={{ height: '6rem' }}></div>
+    </>
+  );
+}
 
 export default function LandingPage() {
   return (
