45644956ea1bddf4e0162627660bbcaf131640d5
nwils000 <nwils000@gmail.com>
2024-06-19 09:29:17 -0400

    tried to fix navbar import into register

---
+++ src/layout/LandingPageNavbar.jsx
@@ -0,0 +1,30 @@
+import '../styles/navbar.css';
+import { Link } from 'react-router-dom';
+import { GiTakeMyMoney } from 'react-icons/gi';
+
+export default function LandingPageNavbar() {
+  return (
+    <div>
+      <div className="navbar-wrapper">
+        <div className="navbar parent-navbar">
+          <Link to="/" className="nav-item">
+            <div className="logo-wrapper">
+              <GiTakeMyMoney className="logo-icon" />
+              <span className="logo-text">Kid Kapital</span>
+            </div>
+          </Link>
+
+          <div className="nav-links-wrapper">
+            <Link className="nav-link landing-page-link" to="/register">
+              Register
+            </Link>
+            <Link to="/login" className="nav-link landing-page-link">
+              Sign in
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div style={{ height: '5rem' }}></div>
+    </div>
+  );
+}
